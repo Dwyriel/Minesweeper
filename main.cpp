ad4e8b5dc3ca5529214e88166962c29f376b0131
Dwyriel <dwyriel333@hotmail.com>
2022-08-27 14:18:43 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <QApplication>
-#include <QMessageBox>
 #include "Source/Window.h"
 
 int main(int argc, char *argv[]) {
+    srand(time(nullptr));
     QApplication app(argc, argv);
     Window window = Window(10, 10, 20, nullptr);
     window.show();
